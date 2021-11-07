8768890944718fc18d1bbf9fcc6001a7df7cd983
Fernando Rojo <frojo@sas.upenn.edu>
2021-11-06 20:38:45 -0400

    chore: example with dev client not working

--- examples/expo/src/App.tsx
+++ examples/expo/src/App.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'zeeg';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  React.useEffect(() => {}, []);
 
   return (
     <View style={styles.container}>
